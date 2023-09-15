f4af438b3e4516972811a771d510f45334523f41
hankyujang <finebears@naver.com>
2023-09-15 20:11:54 +0900

    rename : 경로 수정

--- src/test/java/hankyu/board/spring_board/factory/email/EmailAuthRequestFactory.java
+++ src/test/java/hankyu/board/spring_board/factory/email/EmailAuthRequestFactory.java
@@ -1,4 +1,4 @@
-package hankyu.board.spring_board.factory.dto.member;
+package hankyu.board.spring_board.factory.email;
 
 import hankyu.board.spring_board.dto.email.EmailConfirmRequest;
 
--- src/test/java/hankyu/board/spring_board/service/email/EmailServiceTest.java
+++ src/test/java/hankyu/board/spring_board/service/email/EmailServiceTest.java
@@ -19,10 +19,10 @@ import org.springframework.mail.javamail.JavaMailSender;
 
 import java.util.Optional;
 
-import static hankyu.board.spring_board.factory.dto.member.EmailAuthRequestFactory.createEmailAuthRequest;
-import static hankyu.board.spring_board.factory.dto.member.EmailAuthRequestFactory.createEmailAuthRequestWithInvalidCode;
-import static hankyu.board.spring_board.factory.dto.member.ResendEmailRequestFactory.createResendEmailRequest;
-import static hankyu.board.spring_board.factory.dto.member.ResendEmailRequestFactory.createResendEmailRequestWithEmail;
+import static hankyu.board.spring_board.factory.email.EmailAuthRequestFactory.createEmailAuthRequest;
+import static hankyu.board.spring_board.factory.email.EmailAuthRequestFactory.createEmailAuthRequestWithInvalidCode;
+import static hankyu.board.spring_board.factory.email.ResendEmailRequestFactory.createResendEmailRequest;
+import static hankyu.board.spring_board.factory.email.ResendEmailRequestFactory.createResendEmailRequestWithEmail;
 import static hankyu.board.spring_board.factory.entity.member.MemberFactory.createMember;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
